95d55892b12ff1e1d1a498289ee143572326ddcf
kenley so <kenleybeast1@gmail.com>
2025-02-22 17:46:01 +0800

    added

---
+++ src/main/java/com/example/SpringBatchReview/SpringBatchReviewApplication.java
@@ -0,0 +1,12 @@
+package com.example.SpringBatchReview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchReviewApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchReviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBatchReview/SpringBatchReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBatchReview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
